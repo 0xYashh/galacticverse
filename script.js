a744e7e18573bca7973a4adb4a8f79ec397e8b9c
Yash Kumar <yashkr044@gmail.com>
2024-07-01 14:44:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+        function goToExplore() {
+            window.location.href = 'explore/explore.html';
+        }
+
+
+        function goToJoin() {
+            window.location.href = 'https://forms.fillout.com/t/fPhFpj5KdNus';
+        }
