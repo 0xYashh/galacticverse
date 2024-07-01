73ddf94b989f3a641f3dd38e0f305375c88f552a
Yash Kumar <yashkr044@gmail.com>
2024-07-01 15:12:15 +0530

    update commit

--- script.js
+++ script.js
@@ -6,3 +6,8 @@
         function goToJoin() {
             window.location.href = 'https://forms.fillout.com/t/fPhFpj5KdNus';
         }
+
+        function goToGalacticHQ() {
+            window.location.href = 'hq/g-hq.html';
+        }
+        
