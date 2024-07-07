9abd94c845e1edfe961a71133f15a63920cb9f63
Yash Kumar <yashkr044@gmail.com>
2024-07-07 20:11:26 +0530

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,16 +1,11 @@
-// Load environment variables if running locally
-if (typeof window === 'undefined') {
-    require('dotenv').config();
-  }
-  
-  const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY || 'your_api_key',
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN || 'your_auth_domain',
-    projectId: process.env.FIREBASE_PROJECT_ID || 'your_project_id',
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'your_storage_bucket',
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || 'your_messaging_sender_id',
-    appId: process.env.FIREBASE_APP_ID || 'your_app_id',
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID || 'your_measurement_id'
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY ,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN ,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKE,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID ,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID ,
   };
   
   firebase.initializeApp(firebaseConfig);
